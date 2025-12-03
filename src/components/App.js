d800127c6f9e8e6417883c2952b1d8a659472baa
sahil godwal <godwalsahil786@gmail.com>
2025-12-03 14:05:51 +0000

    fix initial messagea issue another

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
 
 
   async function send(history,message) {
-    const res = await fetch("http://127.0.0.1:8000/api/chat", {
+    const res = await fetch("/api/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
